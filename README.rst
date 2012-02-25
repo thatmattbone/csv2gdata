781ab6373632e57d54a13e96765bde17bc7578d8
Matt Bone <thatmattbone@gmail.com>
2012-02-25 17:24:59 -0600

    updating readme and setup.py

--- README.rst
+++ README.rst
@@ -1,11 +1,25 @@
 
 Installation
-=======
+============
 
-For now::
+If you want to use csv2gdata::
+
+  pip install http://google-visualization-python.googlecode.com/files/gviz_api_py-1.8.0.tar.gz#egg=gviz_api.py
   pip install git+git://github.com/thatmattbone/csv2gdata.git#egg=csv2gdata
 
+(I can't figure out how to get pip to look at my ``dependency_links``
+directive in ``setup.py``. If you find what I've done wrong, you get a
+pony (not really)).
+
+If you want to hack on csv2gdata::
+
+  git clone git://github.com/thatmattbone/csv2gdata.git
+  cd csv2gdata
+  pip install -r requirements.txt
+  #submit pull request :)
 
 
 Some Examples
-==========
+=============
+
+Coming soon...
--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+import distribute_setup
+distribute_setup.use_setuptools()
+
 from setuptools import setup
 
 setup(
@@ -8,16 +12,16 @@ setup(
     author='Matt Bone',
     author_email='thatmattbone@gmail.com',
     url='https://github.com/thatmattbone/csv2gdata',
-    dependency_links = [
-        'http://google-visualization-python.googlecode.com/files/gviz_api_py-1.8.0.tar.gz#egg=gviz_api.py',
-    ],
-    setup_requires=[
-        'gviz_api.py',
-    ],
+    # TODO the gviz_api.py dependency_link trick works when called with
+    # `python setup.py` but NOT when doing something like `pip install csv2gdata-0.0.1.tar.gz`
+    # So until I can figure that out, we'll have to install it manually.
+    #Dependency_links = [
+    #   'http://google-visualization-python.googlecode.com/files/gviz_api_py-1.8.0.tar.gz#egg=gviz_api.py',
+    #],
     install_requires=[
         'csvkit >= 0.4.3',
         'jinja2 >= 2.6',
-        'gviz_api.py',
+        #'gviz_api.py',
     ],
     packages=[
         'csv2gdata',
